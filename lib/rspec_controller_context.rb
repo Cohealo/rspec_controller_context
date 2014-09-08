e3f6045e24b9a003e758a1cf7fa4ecc89687d58f
Andy Hartford <hartforda@gmail.com>
2014-09-07 21:41:11 -0400

    Make requires relative
    since I'm going to vendor this for now.

--- lib/rspec_controller_context.rb
+++ lib/rspec_controller_context.rb
@@ -2,12 +2,11 @@ require "active_support/concern"
 require "active_support/core_ext/class"
 require "active_support/core_ext/hash"
 
-require "rspec_controller_context/version"
-require "rspec_controller_context/class_methods"
-require "rspec_controller_context/configuration"
-require "rspec_controller_context/controller_driver"
-require "rspec_controller_context/keyword_config_parser"
+require File.expand_path("../rspec_controller_context/version", __FILE__)
+require File.expand_path("../rspec_controller_context/class_methods", __FILE__)
+require File.expand_path("../rspec_controller_context/configuration", __FILE__)
+require File.expand_path("../rspec_controller_context/controller_driver", __FILE__)
+require File.expand_path("../rspec_controller_context/keyword_config_parser", __FILE__)
 
 module RspecControllerContext
-  # Your code goes here...
 end
